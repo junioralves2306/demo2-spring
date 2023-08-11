b318057fcf26764f13bf82097e320427711daf2d
Junior Alves <junior.alves2306@gmail.com>
2023-08-11 15:11:20 -0300

    Service layer, component registration

--- src/main/java/com/odenir/curso/repositories/UserRepository.java
+++ src/main/java/com/odenir/curso/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.odenir.curso.entities.User;
 
+//@Repository é opcional neste caso pois a classe já herda a anotação do JpaRepository
 public interface UserRepository extends JpaRepository<User, Long>{
     
 }
