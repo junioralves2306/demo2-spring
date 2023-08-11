7167c4ae1782dad2d4a4d92a9e7dbf6ca4358ab8
Junior Alves <junior.alves2306@gmail.com>
2023-08-11 10:38:08 -0300

    Projeto criado

---
+++ src/test/java/com/odenir/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odenir.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
