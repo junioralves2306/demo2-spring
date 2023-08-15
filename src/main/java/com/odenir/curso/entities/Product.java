0f05e8d49aa4f0830fadd80af70a2bff7d3eafe1
Junior Alves <junior.alves2306@gmail.com>
2023-08-15 10:24:52 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/odenir/curso/entities/Product.java
+++ src/main/java/com/odenir/curso/entities/Product.java
@@ -6,12 +6,15 @@ import java.util.Set;
 
 import org.hibernate.annotations.ManyToAny;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -30,6 +33,10 @@ public class Product implements Serializable{
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name="product_id"),
     inverseJoinColumns =@JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
 
     public Product() {
     }
@@ -86,6 +93,15 @@ public class Product implements Serializable{
         return categories;
     }
 
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -110,6 +126,5 @@ public class Product implements Serializable{
             return false;
         return true;
     }
-
     
 }
--- src/main/java/com/odenir/curso/repositories/ProductRepository.java
+++ src/main/java/com/odenir/curso/repositories/ProductRepository.java
@@ -2,7 +2,6 @@ package com.odenir.curso.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.odenir.curso.entities.Category;
 import com.odenir.curso.entities.Product;
 
 //@Repository é opcional neste caso pois a classe já herda a anotação do JpaRepository
